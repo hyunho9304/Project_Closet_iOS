d9171a892d95af8c4426472d036060493d5612cf
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-06-20 00:52:20 +0900

    APIservice.swift setup

---
+++ Closet_iOS/Closet_iOS/Util/Service/APIservice.swift
@@ -0,0 +1,27 @@
+//
+//  APIservice.swift
+//  Closet_iOS
+//
+//  Created by 박현호 on 2018. 6. 20..
+//  Copyright © 2018년 박현호. All rights reserved.
+//
+
+
+/*
+    Description : 통신으로 사용할 ip 와 port 설정
+
+*/
+
+import Foundation
+
+protocol APIService {
+    
+}
+
+extension APIService {
+    
+    static func url( _ path : String ) -> String {
+        
+        return "http://13.124.195.255:3000" + path
+    }
+}
